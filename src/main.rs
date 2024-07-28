195e8e40b3844d690b988b1e47e6a2e064db8067
CJ <cjgorey132@gmail.com>
2024-07-28 15:23:14 -0500

    added serving text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tower_http::services::{ServeFile, ServeDir};
+
+#[tokio::main]
+async fn main() {
+
+    let static_files = axum::Router::new()
+        .route_service("/", ServeFile::new("../templates/layout.html"));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, static_files).await.unwrap();
+
 }
